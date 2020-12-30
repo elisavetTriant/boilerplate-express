9366f472c38922ec0afd2af12aac6de5b226624d
elisavetTriant <elitriant@gmail.com>
2020-12-30 11:36:52 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,8 +3,13 @@ var app = express();
 
 console.log("Hello World");
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.send('Hello Express');
+});*/
+
+app.get('/', (req, res) => {
+  const absolutePath = __dirname + '/views/index.html'
+  res.sendFile(absolutePath);
 });
 
 
