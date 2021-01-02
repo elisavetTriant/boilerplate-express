841eda11b5c04a432f9e7ef50b60e72cb4a779b8
elisavetTriant <elitriant@gmail.com>
2021-01-02 15:00:44 +0200

    Custom Middleware

--- myApp.js
+++ myApp.js
@@ -7,7 +7,13 @@ console.log("Hello World");
   res.send('Hello Express');
 });*/
 
+const customMiddlware = (req, res, next) => {
+  console.log(req.method+" "+req.path+" - "+req.ip);
+  next();
+}
+
 app.use(express.static(__dirname + '/public'));
+app.use(customMiddlware);
 
 app.get('/', (req, res) => {
   const absolutePath = __dirname + '/views/index.html'
@@ -18,7 +24,7 @@ app.get('/json', (req, res) => {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === 'uppercase')
   	message = message.toUpperCase()
-  
+
   res.json({"message": message});
 });
 
