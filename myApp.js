ee92bc9daa1cd8d96f2d79c6fe99e4a265f7abef
elisavetTriant <elitriant@gmail.com>
2021-01-03 12:28:44 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 
 console.log("Hello World");
 
@@ -14,6 +15,7 @@ const customMiddlware = (req, res, next) => {
 
 app.use(express.static(__dirname + '/public'));
 app.use(customMiddlware);
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   const absolutePath = __dirname + '/views/index.html'
@@ -40,30 +42,17 @@ app.get('/:word/echo', (req, res) => {
   res.json({'echo': word});
 });
 
+app.get('/name', (req, res) => {
+  let firstname = req.query.first;
+  let lastname = req.query.last;
+  res.json({ 'name': firstname+" "+lastname});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.post('/name', (req, res) => {
+  let firstname = req.body.first;
+  let lastname = req.body.last;
+  res.json({ 'name': firstname+" "+lastname});
+});
 
 
 
