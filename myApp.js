325b5fbcc91b209f967e957a5284e4150c4cfcaa
elisavetTriant <elitriant@gmail.com>
2021-01-03 11:47:08 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -28,6 +28,12 @@ app.get('/json', (req, res) => {
   res.json({"message": message});
 });
 
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({time: req.time});
+});
 
 
 
